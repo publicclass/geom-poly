f0925178fea49a9595e1af331fbaa19c7b955003
Robert Sköld <robert@publicclass.se>
2013-04-03 09:41:24 +0200

    Added tests for inverted polygons and static intersecting 'diamonds'.

--- test/test.collides.js
+++ test/test.collides.js
@@ -25,16 +25,16 @@ describe('geom-poly',function(){
 
       var a = poly.make(
         10,10,
-        40,10,
+        10,40,
         40,40,
-        10,40
+        40,10
       )
 
       var b = poly.make(
         20,30,
-        90,30,
+        20,50,
         90,50,
-        20,50
+        90,30
       )
       // console.log(a)
 
@@ -49,7 +49,7 @@ describe('geom-poly',function(){
       // move A out of the way
       var t = c.minTranslationVector
       poly.translate(a,t[0],t[1])
-      // console.log(a)
+      draw.poly(a).stroke('lightblue')
 
       // and try again
       var c = poly.collides(a,b,v)
@@ -57,7 +57,6 @@ describe('geom-poly',function(){
       expect(c.willIntersect).to.equal(false)
       expect(c.intersect).to.equal(false)
 
-      draw.poly(a).stroke('pink')
     })
 
     /**
@@ -79,28 +78,29 @@ describe('geom-poly',function(){
 
       var a = poly.make(
         10,10,
-        40,10,
+        10,40,
         40,40,
-        10,40
+        40,10
       )
 
       var b = poly.make(
         20,50,
-        90,50,
+        20,70,
         90,70,
-        20,70
+        90,50
       )
       var av = vec.make(0,11)
       var bv = vec.make(0,0)
       var v = vec.sub(av,bv)
       var c = poly.collides(a,b,v)
 
+      draw.poly(b).stroke('red')
+      draw.poly(a).stroke('blue')
+
       // console.log(c)
       expect(c.willIntersect).to.equal(true)
       expect(c.intersect).to.equal(false)
 
-      draw.poly(b).stroke('red')
-      draw.poly(a).stroke('blue')
     })
 
     /**
@@ -121,28 +121,29 @@ describe('geom-poly',function(){
     it('will only touch (not intersect) with vertical velocity',function(){
       var a = poly.make(
         10,10,
-        40,10,
+        10,40,
         40,40,
-        10,40
+        40,10
       )
 
       var b = poly.make(
         20,50,
-        90,50,
+        20,70,
         90,70,
-        20,70
+        90,50
       )
       var av = vec.make(0,10)
       var bv = vec.make(0,0)
       var v = vec.sub(av,bv)
       var c = poly.collides(a,b,v)
 
+      draw.poly(b).stroke('red')
+      draw.poly(a).stroke('blue')
+
       // console.log(c)
       expect(c.willIntersect).to.equal(false)
       expect(c.intersect).to.equal(false)
 
-      draw.poly(b).stroke('red')
-      draw.poly(a).stroke('blue')
     })
 
 
@@ -181,6 +182,12 @@ describe('geom-poly',function(){
       var v = vec.sub(av,bv)
       var c = poly.collides(a,b,v)
 
+      draw.poly(b).stroke('red')
+      draw.poly(a).stroke('blue')
+
+      poly.translate(a,av[0],av[1])
+      draw.poly(a).stroke('lightblue')
+
       // console.log(c)
       expect(c.willIntersect).to.equal(true)
       expect(c.intersect).to.equal(false)
@@ -189,8 +196,6 @@ describe('geom-poly',function(){
       //      to A while it still "flies through". I'd like to be able to know
       //      "when" it would intersect so I can avoid it.
 
-      draw.poly(b).stroke('red')
-      draw.poly(a).stroke('blue')
     })
 
 
@@ -225,12 +230,15 @@ describe('geom-poly',function(){
       var v = vec.sub(av,bv)
       var c = poly.collides(a,b,v)
 
+      draw.poly(b).stroke('red')
+      draw.poly(a).stroke('blue')
+
       // console.log(c)
       expect(c.willIntersect).to.equal(true)
       expect(c.intersect).to.equal(false)
 
-      draw.poly(b).stroke('red')
-      draw.poly(a).stroke('blue')
+      poly.translate(a,v[0],v[1])
+      draw.poly(a).stroke('lightblue')
 
       // TODO the minTranslationVector is now [0,-10] which will push B next
       //      to A while it still "flies through". I'd like to be able to know
@@ -264,21 +272,20 @@ describe('geom-poly',function(){
 
       var a = poly.make(
         10,10,
-        40,10,
+        10,40,
         40,40,
-        10,40
+        40,10
       )
 
       var b = poly.make(
         20,40,
-        90,40,
+        20,60,
         90,60,
-        20,60
+        90,40
       )
 
-      var v = vec.make()
-      var c = poly.collides(a,b,v)
-      // console.log(a)
+      var c = poly.collides(a,b)
+
       // console.log(c)
       expect(c.willIntersect).to.equal(false)
       expect(c.intersect).to.equal(false)
@@ -308,28 +315,31 @@ describe('geom-poly',function(){
 
       var a = poly.make(
         10,10,
-        40,10,
+        10,40,
         40,40,
-        10,40
+        40,10
       )
 
       var b = poly.make(
         20,50,
-        90,50,
+        20,70,
         90,70,
-        20,70
+        90,50
       )
       var av = vec.make(0,-100)
       var bv = vec.make(0,0)
       var v = vec.sub(av,bv)
       var c = poly.collides(a,b,v)
 
+      draw.poly(b).stroke('red')
+      draw.poly(a).stroke('blue')
+
       // console.log(c)
       expect(c.willIntersect).to.equal(false)
       expect(c.intersect).to.equal(false)
 
-      draw.poly(b).stroke('red')
-      draw.poly(a).stroke('blue')
+      poly.translate(a,av[0],av[1])
+      draw.poly(a).stroke('lightblue')
     })
 
 
@@ -351,32 +361,35 @@ describe('geom-poly',function(){
      *
      */
 
-    it('will collide with non-axis-aligned polygons too',function(){
+    it('will collide with non-axis-aligned polygons',function(){
 
       var a = poly.make(
         50,30,
-        70,50,
-        50,30,
-        70,10
+        30,50,
+        50,70,
+        70,50
       )
 
       var b = poly.make(
-        20,50,
-        90,50,
-        90,70,
-        20,70
+        50,70,
+        50,90,
+        70,90,
+        70,70
       )
       var av = vec.make(0,0)
       var bv = vec.make(0,-10)
       var v = vec.sub(av,bv)
       var c = poly.collides(a,b,v)
 
-      // console.log(c)
+      draw.poly(a).stroke('blue')
+      draw.poly(b).stroke('red')
+
+      poly.translate(b,bv[0],bv[1])
+      draw.poly(b).stroke('pink')
+
+      console.log(c)
       expect(c.willIntersect).to.equal(true)
       expect(c.intersect).to.equal(false)
-
-      draw.poly(b).stroke('red')
-      draw.poly(a).stroke('blue')
     })
 
     /**
@@ -396,32 +409,161 @@ describe('geom-poly',function(){
      *
      */
 
-    it('will collide with non-axis-aligned polygons too (reversed)',function(){
+    it('will collide with non-axis-aligned polygons (reversed)',function(){
 
       var b = poly.make(
         50,30,
-        70,50,
-        50,30,
-        70,10
+        30,50,
+        50,70,
+        70,50
       )
 
       var a = poly.make(
-        20,50,
-        90,50,
-        90,70,
-        20,70
+        55,70,
+        55,90,
+        75,90,
+        75,70
       )
       var av = vec.make(0,-10)
       var bv = vec.make(0,0)
       var v = vec.sub(av,bv)
       var c = poly.collides(a,b,v)
 
+      draw.poly(b).stroke('red')
+      draw.poly(a).stroke('blue')
+
+      poly.translate(a,av[0],av[1])
+      draw.poly(a).stroke('lightblue')
+
       // console.log(c)
       expect(c.willIntersect).to.equal(true)
       expect(c.intersect).to.equal(false)
 
+    })
+
+    /**
+     *
+     *    A v = 0,0
+     *   / \
+     *  /   \
+     * +  +-B++ v = 0,0
+     *  \ |   |
+     *   \|   |
+     *    +---+
+     *
+     */
+
+    it('intersecting non-axis-aligned polygons',function(){
+
+      var a = poly.make(
+        50,60,
+        30,80,
+        50,100,
+        70,80
+      )
+
+      var b = poly.make(
+        55,55,
+        55,75,
+        75,75,
+        75,55
+      )
+      var c = poly.collides(a,b)
+
+      draw.poly(a).stroke('blue')
+      draw.poly(b).stroke('red')
+
+      console.log(c)
+      expect(c.willIntersect).to.equal(true)
+      expect(c.intersect).to.equal(true)
+    })
+
+
+    /**
+     *
+     *    A v = 0,0
+     *   / \
+     *  /   \
+     * +  +-B++ v = 0,0
+     *  \ |   |
+     *   \|   |
+     *    +---+
+     *
+     */
+
+    it('intersecting non-axis-aligned polygons (inverted)',function(){
+
+      var a = invert(poly.make(
+        50,30,
+        30,50,
+        50,70,
+        70,50
+      ))
+
+      var b = invert(poly.make(
+        55,55,
+        55,75,
+        75,75,
+        75,55
+      ))
+      var c = poly.collides(a,b)
+
+      draw.poly(a).stroke('blue')
+      draw.poly(b).stroke('red')
+
+      console.log(c)
+      expect(c.willIntersect).to.equal(true)
+      expect(c.intersect).to.equal(true)
+    })
+
+
+    /**
+     *
+     *          B v = 0,0
+     *         / \
+     *        /   \
+     *       +     +
+     *        \   /
+     *         \ /
+     *          +
+     *
+     *    +-A-+ v = 0,-10
+     *    |   |
+     *    |   |
+     *    +---+
+     *
+     */
+
+    it('will collide with non-axis-aligned polygons too on the left side',function(){
+
+      var b = poly.make(
+        75,30,
+        55,50,
+        75,70,
+        95,50
+      )
+
+      var a = poly.make(
+        50,70,
+        50,90,
+        70,90,
+        70,70
+      )
+      var av = vec.make(0,-10)
+      var bv = vec.make(0,0)
+      var v = vec.sub(av,bv)
+      var c = poly.collides(a,b,v)
+
       draw.poly(b).stroke('red')
       draw.poly(a).stroke('blue')
+
+      poly.translate(a,av[0],av[1])
+      draw.poly(a).stroke('lightblue')
+
+      // console.log(c)
+      expect(c.willIntersect).to.equal(true)
+      expect(c.intersect).to.equal(false)
+
     })
 
     /**
@@ -439,15 +581,15 @@ describe('geom-poly',function(){
 
       var b = poly.make(
         50,30,
-        65,50,
-        35,50
+        35,50,
+        65,50
       )
 
       var a = poly.make(
         50,40,
-        70,40,
+        50,60,
         70,60,
-        50,60
+        70,40
       )
       var av = vec.make(0,0)
       var bv = vec.make(0,0)
@@ -510,7 +652,7 @@ describe('geom-poly',function(){
       expect(c.intersect).to.equal(false)
 
       poly.translate(a,av[0],av[1])
-      draw.poly(a).stroke('pink')
+      draw.poly(a).stroke('lightblue')
     })
   })
 })
@@ -530,4 +672,14 @@ function createDraw(){
     };
     return draw;
   }
+}
+
+function invert(p){
+  var q = poly.make();
+  for(var i=p.length-1; i>=0; i--){
+    var v = p.vertices[i];
+    poly.add(q,v[0],v[1]);
+  }
+  poly.close(q)
+  return q;
 }
