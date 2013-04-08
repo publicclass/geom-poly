e4cfd47e263d25f8b9889999b3ca496b4f1b61fd
Robert Sköld <robert@publicclass.se>
2013-04-08 10:24:32 +0200

    Added some tests with a hexagon.

--- test/test.simple.js
+++ test/test.simple.js
@@ -78,6 +78,22 @@ describe('geom-poly',function(){
       poly.free(p)
     })
 
+    it('should properly compute the center of a hexagon',function(){
+      var p = poly.make()
+      var a = 2 * Math.PI / 6
+        , w = 12;
+      for(var i=5; i >= 0; i--){
+        poly.add(p, w * Math.cos(i * a), w * Math.sin(i * a) );
+      }
+      poly.close(p)
+      var c = poly.centroid(p);
+      // round to 15 digits to ignore floating errors
+      expect(Math.round(c[0]*1e15)).to.eql(0)
+      expect(Math.round(c[1]*1e15)).to.eql(0)
+      poly.free(p)
+      vec.free(c)
+    })
+
     it('should properly compute the center of a irregular polygon',function(){
       var p = poly.make(
         5,5,
@@ -87,7 +103,7 @@ describe('geom-poly',function(){
         5,12
       )
       p = poly.reverse(p)
-      expect(poly.centroid(p)).to.eql([9.8,8.5])
+      expect(poly.centroid(p)).to.eql([9.8,8.5]) // according to wolfram alpha
       poly.free(p)
     })
 
@@ -101,7 +117,7 @@ describe('geom-poly',function(){
         459,369
       )
       p = poly.reverse(p)
-      expect(poly.centroid(p)).to.eql([500,457])
+      expect(poly.centroid(p)).to.eql([500,457]) // according to wolfram alpha
       poly.free(p)
     })
   })
@@ -128,4 +144,39 @@ describe('geom-poly',function(){
       expect(poly.centroid(p)).to.eql([500,457])
     })
   })
+
+  describe('a generated hexagon with 12px edge length',function(){
+    var p = poly.make()
+    var a = 2 * Math.PI / 6
+      , w = 12;
+    for(var i=5; i >= 0; i--){
+      poly.add(p, w * Math.cos(i * a), w * Math.sin(i * a) );
+    }
+    poly.close(p)
+
+    it('should give the right perimeter',function(){
+      expect(poly.perimeter(p)).to.equal(72)
+    })
+
+    it('should give the right radius',function(){
+      // round to 12 digits to ignore floating errors
+      var r = poly.radius(p);
+      expect(round(r,1e12)).to.equal(12)
+    })
+
+    it('should give the right area',function(){
+      expect(poly.area(p)).to.equal(374.1229744348775)
+    })
+
+    it('should give the right centroid',function(){
+      // round to 15 digits to ignore floating errors
+      var c = poly.centroid(p);
+      expect(round(c[0],1e15)).to.eql(0)
+      expect(round(c[1],1e15)).to.eql(0)
+    })
+  })
 })
+
+function round(i,n){
+  return Math.round(i*n)/n;
+}
