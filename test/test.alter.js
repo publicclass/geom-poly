cb49c15ab238bcc46e1b9365e3a3eb3e61b0495d
Robert Sköld <robert@publicclass.se>
2013-04-25 15:17:12 +0200

    Added a few tests for poly.transform()

--- test/test.alter.js
+++ test/test.alter.js
@@ -2,6 +2,7 @@ var isNode = typeof __dirname != 'undefined';
 
 var poly = require(isNode ? '../' : 'geom-poly')
   , vec = require(isNode ? 'geom-vec' : 'publicclass-geom-vec')
+  , mat = require(isNode ? 'geom-mat' : 'publicclass-geom-mat')
   , expect = expect || require('expect.js');
 
 poly.verbose = vec.verbose = false;
@@ -63,6 +64,95 @@ describe('geom-poly',function(){
       }).to.throwException(/not supported/)
     })
 
+  })
+
+  describe('transform()',function(){
+
+    it('should scale a rect x2',function(){
+      var rect = poly.make(0,1,1,1,1,0,0,0)
+
+      expect(poly.aabb(rect)).to.eql([0,1,1,0])
+
+      var m = mat.scale(2,2);
+      poly.transform(rect,m)
+
+      expect(poly.aabb(rect)).to.eql([0,2,2,0])
+    })
+
+    it('should scale a rect x.5',function(){
+      var rect = poly.make(0,2,2,2,2,0,0,0)
+
+      expect(poly.aabb(rect)).to.eql([0,2,2,0])
+
+      var m = mat.scale(.5,.5);
+      poly.transform(rect,m)
+
+      expect(poly.aabb(rect)).to.eql([0,1,1,0])
+    })
+
+    it('should scale a rect twice',function(){
+      var rect = poly.make(0,1,1,1,1,0,0,0)
+
+      expect(poly.aabb(rect)).to.eql([0,1,1,0])
+
+      var m = mat.scale(2,2)
+      poly.transform(rect,m)
+      poly.transform(rect,m)
+
+      expect(poly.aabb(rect)).to.eql([0,4,4,0])
+    })
+
+    it('should scale a rect twice on x',function(){
+      var rect = poly.make(0,1,1,1,1,0,0,0)
+
+      expect(poly.aabb(rect)).to.eql([0,1,1,0])
+
+      var m = mat.scale(2,1)
+      poly.transform(rect,m)
+      poly.transform(rect,m)
+
+      expect(poly.aabb(rect)).to.eql([0,4,1,0])
+    })
+
+    it('should scale a rect twice on y',function(){
+      var rect = poly.make(0,1,1,1,1,0,0,0)
+
+      expect(poly.aabb(rect)).to.eql([0,1,1,0])
+
+      var m = mat.scale(1,2)
+      poly.transform(rect,m)
+      poly.transform(rect,m)
+
+      expect(poly.aabb(rect)).to.eql([0,1,4,0])
+    })
+
+    it('should scale a rect from center',function(){
+      var rect = poly.make(0,1,1,1,1,0,0,0)
+
+      expect(poly.aabb(rect)).to.eql([0,1,1,0])
+
+      var m = mat.translate(-0.5,-0.5)
+      mat.scale(2,2,m)
+      mat.translate(0.5,0.5,m)
+
+      poly.transform(rect,m)
+
+      expect(poly.aabb(rect)).to.eql([-0.5,1.5,1.5,-0.5])
+    })
+
+    it('should scale a rect from right',function(){
+      var rect = poly.make(0,1,1,1,1,0,0,0)
+
+      expect(poly.aabb(rect)).to.eql([0,1,1,0])
+
+      var m = mat.translate(-1,-1)
+      mat.scale(2,2,m)
+      mat.translate(1,1,m)
+
+      poly.transform(rect,m)
+
+      expect(poly.aabb(rect)).to.eql([-1,1,1,-1])
+    })
   })
 
 })
